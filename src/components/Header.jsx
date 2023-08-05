937348d2774310aafc1d907190c6347db7610d45
Sushant Pandey <sushhantpandey@gmail.com>
2023-08-05 19:00:13 +0530

    Bug fixes and Improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header className="border-b border-b-slate-500 select-none mb-4">
             <h1 className={`${raleway.className} bg-green-900 text-center text-xl p-4`}>
-                Data Visualizaton Dashboard
+                Data Visualization Dashboard
             </h1>
         </header>
     )
