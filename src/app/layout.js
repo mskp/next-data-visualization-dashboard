eb8af4a14edab44f45aeb0f07fbbeb32b49b2fb1
Sushant Pandey <sushhantpandey@gmail.com>
2023-08-05 16:15:27 +0530

    Info not provided by the developer

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Data Visualization Dahsboard',
+  title: 'Data Visualization Dashboard',
 }
 
 export default function RootLayout({ children }) {
